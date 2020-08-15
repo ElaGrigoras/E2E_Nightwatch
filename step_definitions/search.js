cf32e9853dd49bae05baed9e789203866ec473f7
Miha Grigoras <mihaela@eved.com>
2020-08-15 20:36:52 +0300

    step definition added

--- step_definitions/search.js
+++ step_definitions/search.js
@@ -1,8 +1,16 @@
 const { client } = require('nightwatch-api')
 const { Given, Then, When } = require('cucumber')
-const homePage = require('../pages/home_page')
+const navPage = require('../pages/navigation')
+const homePage = require('../pages/home')
 
-Given(/^I go to pearson.com main page$/, () => {
-  siteNavigationPage.navToMainPage()
-  return client
+Given(/^I go to pearson.com main page$/, async () => {
+  return await client.url('https://www.pearson.com/')
+})
+
+Then(/^The search input exists$/, async () => {
+  return await client.assert.visible(homePage.elements.searchInput)
+})
+
+Given(/^I search by "(.*)"$/, async (searchTerm) => {
+  return await homePage.searchBy(searchTerm)
 })
